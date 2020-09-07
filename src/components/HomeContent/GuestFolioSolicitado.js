d158bf082e16ecc5da546fc2c6ffb7ce64c51d31
Esther Manrique <esmango0610@gmail.com>
2020-09-07 00:46:40 -0500

    Add Guest component  and update components

---
+++ src/components/HomeContent/GuestFolioSolicitado.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles({
+    container: {
+      width: '60%', 
+      padding: '15px 15px 100px', 
+      marginLeft: '25px',
+      borderRadius: '8px',
+      
+    },
+    tableRow: {
+      border: 'solid',
+      borderColor: '#CAF199',
+  }
+  });
+
+const createData = (FolioID, Company, Status) => {
+    return { FolioID, Company, Status };
+  }
+  
+  const rows = [
+    createData('Transpotes Delux', 'Jorge Estrada', 'Pnding'),
+
+  ];
+
+const GuestFolioSolicitado = () => {
+    const classes = useStyles();
+    return ( 
+          <TableContainer component={Paper} className={classes.container}>
+          <Typography>Folio Solicitado</Typography>
+            <Table aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center">Folio ID</TableCell>
+                  <TableCell align="center">Company</TableCell>
+                  <TableCell align="center">Status</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody border='1px solid green'>
+                {rows.map((row) => (
+                  <TableRow key={row.FolioID} className={classes.tableRow}>
+                    <TableCell align="center" component="th" scope="row">
+                      {row.FolioID}
+                    </TableCell>
+                    <TableCell align="center">{row.Company}</TableCell>
+                    <TableCell align="center">{row.Status}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+     );
+}
+ 
+export default GuestFolioSolicitado;
